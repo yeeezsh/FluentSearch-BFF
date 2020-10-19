8d5b3b8059ad781a91219b9d519b657a94fec5d2
bxine <sine.narisa@gmail.com>
2020-10-19 15:27:52 +0700

    test app service

--- user/src/app.service.ts
+++ user/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './CreateUserDto.dto';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class UserService {
+  getHello(): CreateUserDto {
+    const cat = {
+      "name": "cat",
+      "age": 10,
+      "breed": "milk"
+    }
+    return cat;
   }
 }
