70be976f75e375bb0a3ad3967b517eb98654a777
bxine <sine.narisa@gmail.com>
2020-10-19 15:26:50 +0700

    test app controller

--- user/src/app.controller.spec.ts
+++ user/src/app.controller.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './app.controller';
+import { UserService } from './app.service';
 
 describe('AppController', () => {
-  let appController: AppController;
+  let appController: UserController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = app.get<UserController>(UserController);
   });
 
   describe('root', () => {
