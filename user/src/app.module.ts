38c277e645cb9e7757749728792532d7d04097ae
bxine <sine.narisa@gmail.com>
2020-10-19 15:28:25 +0700

    Edit module for test

--- user/src/app.module.ts
+++ user/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './app.controller';
+import { UserService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
