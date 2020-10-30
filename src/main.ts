4342abf8cf145f262d770040887b6077ec14f57f
Korbboon <korbboon.thuswongsa@agoda.com>
2020-10-31 03:11:23 +0700

    Setup swagger page

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Fluent Search BFF')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
 
   await app.listen(5000);
 }
