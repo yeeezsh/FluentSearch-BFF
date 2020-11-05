84fdc97450582b0ec3155507d268f18ff8218f20
yee2542 <yee2542gtl@gmail.com>
2020-11-06 00:38:12 +0700

    Implement database module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { UserController } from './user.controller';
+import { userProivders } from './user.providers';
 import { UserService } from './user.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, ...userProivders],
 })
 export class UserModule {}
