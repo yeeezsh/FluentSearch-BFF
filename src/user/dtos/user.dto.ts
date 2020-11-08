64ba27824fd4a929bf028d6c1c997b4902bc2988
bxine <sine.narisa@gmail.com>
2020-11-08 21:46:15 +0700

    Add user dto

---
+++ src/user/dtos/user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'test' })
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
