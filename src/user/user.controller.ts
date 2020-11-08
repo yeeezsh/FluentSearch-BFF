fe0d5b17c03215509ff7524a5d988510791ff0bd
bxine <sine.narisa@gmail.com>
2020-11-08 22:56:30 +0700

    Add promise

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/user.dto';
+import { UserDoc } from './interfaces/user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,9 +8,10 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getAllUser() {
+  async getAllUser(): Promise<UserDoc[]> {
     return this.userService.getAllUser();
   }
+
   @Post('/user')
   async createUser(@Body() body: CreateUserDto) {
     return this.userService.createUser(body);
