258605b6d40bf1848939a156989b9ebe07d8785e
bxine <sine.narisa@gmail.com>
2020-11-08 21:43:09 +0700

    Add @post to create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('/user')
+  async createUser(@Body() body: CreateUserDto) {
+    return this.userService.creatUser(body);
+  }
+}
