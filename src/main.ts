ed0e39f3a95623a9ccb049fb1eb4d6aade6b528c
yee2542 <yee2542gtl@gmail.com>
2020-11-10 00:53:21 +0700

    WIP inplement config on main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigurationInterface } from './config/config.interface';
+import { APP_CONFIG, ConfigModule } from './config/config.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe(),
-  );
+  const config: ConfigurationInterface = app
+    .select(ConfigModule)
+    .get(APP_CONFIG);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const options = new DocumentBuilder()
     .setTitle('Fluent Search BFF')
