031d75c81c923ab30646098fdbeb35587b88e3f4
yee2542 <yee2542gtl@gmail.com>
2020-11-09 22:35:14 +0700

    Update mock config

--- src/database/mock-database.providers.ts
+++ src/database/mock-database.providers.ts
@@ -1,15 +1,17 @@
 import { Provider } from '@nestjs/common';
-import { MongoMemoryReplSet } from 'mongodb-memory-server';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as mongoose from 'mongoose';
 import { DATABASE_CONNECTION } from './constants/database.constant';
+
+const replSet = new MongoMemoryServer();
 
 export const mockDatabaseProviders: Provider[] = [
   {
     provide: DATABASE_CONNECTION,
     useFactory: async (): Promise<typeof mongoose> => {
-      const replSet = new MongoMemoryReplSet();
-      await replSet.waitUntilRunning();
       const uri = await replSet.getUri();
+      const onJest = process.env.JEST_WORKER_ID !== undefined;
+      console.log('Running on Jest: ', onJest);
       console.log('Mock replica URI: ', uri);
       return await mongoose.connect(uri, {
         useNewUrlParser: true,
