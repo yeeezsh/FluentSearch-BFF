8bf83adf4d62eca7bccbc8127aec0ecd46073a34
yee2542 <yee2542gtl@gmail.com>
2020-11-09 23:05:05 +0700

    Add mocking database provider

--- src/database/mock-database.providers.ts
+++ src/database/mock-database.providers.ts
@@ -1,22 +1,13 @@
 import { Provider } from '@nestjs/common';
-import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as mongoose from 'mongoose';
+import { mockDatabaseFactory } from '../utils/mock-database.factory';
 import { DATABASE_CONNECTION } from './constants/database.constant';
-
-const replSet = new MongoMemoryServer();
 
 export const mockDatabaseProviders: Provider[] = [
   {
     provide: DATABASE_CONNECTION,
     useFactory: async (): Promise<typeof mongoose> => {
-      const uri = await replSet.getUri();
-      const onJest = process.env.JEST_WORKER_ID !== undefined;
-      console.log('Running on Jest: ', onJest);
-      console.log('Mock replica URI: ', uri);
-      return await mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      return mockDatabaseFactory();
     },
   },
 ];
