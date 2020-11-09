5200bf1f093bb6b69b2ef0fc4670acc076797df3
yee2542 <yee2542gtl@gmail.com>
2020-11-09 23:07:07 +0700

    Implement database connection

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { DATABASE_CONNECTION } from '../src/database/constants/database.constant';
+import { mockDatabaseFactory } from '../src/utils/mock-database.factory';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -9,7 +11,12 @@ describe('AppController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(DATABASE_CONNECTION)
+      .useFactory({
+        factory: async () => mockDatabaseFactory(),
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
