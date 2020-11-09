898b556fb32b441855b3c7a00e4e701326eb7588
yee2542 <yee2542gtl@gmail.com>
2020-11-09 23:36:34 +0700

    Use logger instead console

--- src/utils/mock-database.factory.ts
+++ src/utils/mock-database.factory.ts
@@ -6,8 +6,8 @@ const replSet = new MongoMemoryServer();
 export const mockDatabaseFactory = async (): Promise<typeof mongoose> => {
   const uri = await replSet.getUri();
   const onJest = process.env.JEST_WORKER_ID !== undefined;
-  console.log('Running on Jest: ', onJest);
-  console.log('Mock replica URI: ', uri);
+  Logger.log(`Running on Jest: ${onJest}`);
+  Logger.log(`Mock replica URI: ${uri}`);
   return await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
