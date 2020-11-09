3d2fe955e329b02096c23648dfee76147bba849e
yee2542 <yee2542gtl@gmail.com>
2020-11-10 00:46:30 +0700

    Export provider

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { configProviders } from './config.providers';
 
-@Module({})
+@Module({
+  providers: [...configProviders],
+  exports: [...configProviders],
+})
 export class ConfigModule {}
