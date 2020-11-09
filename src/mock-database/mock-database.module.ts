4a9f671f27dbd2dc1572f4c7b71d6a1e8f3cd5da
yee2542 <yee2542gtl@gmail.com>
2020-11-09 21:39:21 +0700

    Deprecated mock-database module

--- src/mock-database/mock-database.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { mockDatabaseProviders } from './mock-database.providers';
-
-@Module({
-  providers: [...mockDatabaseProviders],
-})
-export class MockDatabaseModule {}
