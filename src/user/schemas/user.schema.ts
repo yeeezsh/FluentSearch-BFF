01b582d0d5ef4e74d27995a34b3e19db932469eb
yee2542 <yee2542gtl@gmail.com>
2020-11-11 18:36:59 +0700

    Implement package field

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,5 +1,9 @@
 import { Schema } from 'mongoose';
-import { userRoleConstant } from './enums/user-role.enum';
+import {
+  userPackageConstant,
+  UserPackageEnum,
+} from './enums/user-package.enum';
+import { userRoleConstant, UserRoleEnum } from './enums/user-role.enum';
 import { userTokenSchema } from './user-token.schema';
 
 const userSchema = new Schema({
@@ -10,7 +14,8 @@ const userSchema = new Schema({
 
   // logic
 
-  role: { type: String, required: true, enum: userRoleConstant },
+  role: { type: UserRoleEnum, required: true, enum: userRoleConstant },
+  package: { type: UserPackageEnum, required: true, enum: userPackageConstant },
 
   //   meta
   createDate: { type: Date, default: Date.now, required: true },
