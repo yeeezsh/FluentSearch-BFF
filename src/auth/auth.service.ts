2e7ec0f16ed5b815d4d4a4abea1d68860d71147a
bxine <sine.narisa@gmail.com>
2020-11-11 18:08:02 +0700

    WIP: Edit valid user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,12 +6,14 @@ export class AuthService {
   constructor(private readonly userService: UserService) {}
 
   async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.userService.loginUser({ email, password });
-
-    // if (user && user.password === password) {
-    //   const { password, ...result } = user;
-    //   return result;
-    // }
+    const user = await this.userService.loginUser({
+      email,
+      password: password,
+    });
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
     return null;
   }
 }
