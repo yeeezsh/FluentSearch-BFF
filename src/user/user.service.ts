4ff666eee82cc88a27e4a0c33571523381688159
yee2542 <yee2542gtl@gmail.com>
2020-11-11 19:16:46 +0700

    Implement findOne

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,14 +4,18 @@ import { Model } from 'mongoose';
 import { USER_MODEL } from './constants/user.provider.constant';
 import { CreateUserDto } from './dtos/user.dto';
 import { User, UserDoc } from './interfaces/user.interface';
+import { UserPackageEnum } from './schemas/enums/user-package.enum';
 import { UserRoleEnum } from './schemas/enums/user-role.enum';
 
 @Injectable()
 export class UserService {
   constructor(@Inject(USER_MODEL) private readonly userModel: Model<UserDoc>) {}
 
-  findOne(_id: string) {
-    throw new Error('Method not implemented.');
+  async findOne(id: string): Promise<Omit<User, 'password'> | null> {
+    return this.userModel
+      .findById(id)
+      .select({ password: -1 })
+      .lean();
   }
 
   async getUsers(skip = 0, limit = 1000): Promise<UserDoc[]> {
@@ -32,11 +36,13 @@ export class UserService {
 
     const user: User = {
       ...create,
-      role: UserRoleEnum.user,
       password: await hash(create.password, 'verystrongsalt@123'),
+      oauth: [],
+      role: UserRoleEnum.user,
+      package: UserPackageEnum.freeUser,
+
       createDate: new Date(),
       updateDate: new Date(),
-      oauth: [],
     };
 
     const doc = new this.userModel(user);
