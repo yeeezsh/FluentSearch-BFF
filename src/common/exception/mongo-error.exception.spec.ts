e1618576976daa2df3e8d13d33a70effbc256db2
yee2542 <yee2542gtl@gmail.com>
2020-11-11 21:45:46 +0700

    Add un-structure data parsing

--- src/common/exception/mongo-error.exception.spec.ts
+++ src/common/exception/mongo-error.exception.spec.ts
@@ -2,27 +2,33 @@ import { MongoHandlingEnum } from './@enums/mongo-handling.enum';
 import { MongoErrorMessageType } from './@types/mongo-error.type';
 import { MongoErrorException } from './mongo-error.exception';
 
-describe('MongoErrorException Tests', () => {
-  const mockCanParsingErrorMsg: MongoErrorMessageType = {
-    name: 'test',
-    index: 0,
-    driver: true,
-    code: MongoHandlingEnum.IndexDuplicated,
-  };
-  const mockCannotParsingErrorMsg: MongoErrorMessageType = {
-    name: 'test',
-    index: 0,
-    driver: true,
-    code: 12345,
-  };
+export const mockIndexDuplicatedErrorMsg: MongoErrorMessageType = {
+  name: 'MongoError',
+  index: 0,
+  driver: true,
+  code: MongoHandlingEnum.IndexDuplicated,
+};
+const mockUnhandlingErrorMsg: MongoErrorMessageType = {
+  name: 'MongoError',
+  index: 0,
+  driver: true,
+  code: 12345,
+};
+const mockNotInstanceOfMongoErrorMsg = 'test message';
 
+describe('MongoErrorException Tests', () => {
   it('Should parsed correctly', () => {
-    const parsed = new MongoErrorException(mockCanParsingErrorMsg);
+    const parsed = new MongoErrorException(mockIndexDuplicatedErrorMsg);
     expect(parsed.type).toEqual(MongoHandlingEnum.IndexDuplicated);
   });
 
   it('Should return -1 when cannot parsed', () => {
-    const parsed = new MongoErrorException(mockCannotParsingErrorMsg);
+    const parsed = new MongoErrorException(mockUnhandlingErrorMsg);
+    expect(parsed.type).toEqual(MongoHandlingEnum.NotParsed);
+  });
+
+  it('Should return -1 when error is not instanceof MongoErrorMessageType', () => {
+    const parsed = new MongoErrorException(mockNotInstanceOfMongoErrorMsg);
     expect(parsed.type).toEqual(MongoHandlingEnum.NotParsed);
   });
 });
