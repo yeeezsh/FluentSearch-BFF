0fab459fdf75b7fff985b14887140f94d3902b88
yee2542 <yee2542gtl@gmail.com>
2020-11-11 19:34:47 +0700

    Fix bad type

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersQuery } from './@types/user.query.types';
 import { CreateUserDto } from './dtos/user.dto';
-import { UserDoc } from './interfaces/user.interface';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -8,12 +8,13 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getUsers(): Promise<UserDoc[]> {
+  async getUsers(): Promise<UsersQuery> {
     return this.userService.getUsers();
   }
 
   @Post('/user')
-  async createUser(@Body() body: CreateUserDto) {
-    return this.userService.createUser(body);
+  async createUser(@Body() body: CreateUserDto): Promise<void> {
+    await this.userService.createUser(body);
+    return;
   }
 }
