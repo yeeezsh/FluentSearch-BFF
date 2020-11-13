dfc72ead05dc8d25cfc2e96c458653eba4d6ad2a
bxine <sine.narisa@gmail.com>
2020-11-13 19:58:36 +0700

    WIP add login user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { AuthService } from '../common/exception/authentication/auth.service';
 import { UsersQuery } from './@types/user.query.types';
 import { CreateUserDto } from './dtos/user.dto';
+import { UserLoginDto } from './dtos/user.login.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   async getUsers(): Promise<UsersQuery> {
@@ -15,6 +20,15 @@ export class UserController {
   @Post('/user')
   async createUser(@Body() body: CreateUserDto): Promise<void> {
     await this.userService.createUser(body);
+    return;
+  }
+
+  @Post('/auth/login')
+  //TODO: cookie
+  async loginUser(@Body() body: UserLoginDto, @Res() res: Response) {
+    const { email, password } = body;
+    const user = await this.authService.validateUser(email, password);
+    //const cookie = this.authService.getCookie(user._id);
     return;
   }
 }
