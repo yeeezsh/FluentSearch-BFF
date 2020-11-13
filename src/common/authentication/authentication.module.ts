9295b77e86637a086222b8c2cd2b8e9a2cfd83da
bxine <sine.narisa@gmail.com>
2020-11-13 22:04:31 +0700

    Add jwtModule.register

--- src/common/authentication/authentication.module.ts
+++ src/common/authentication/authentication.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../../user/user.module';
 import { userProivders } from '../../user/user.providers';
+import { jwtConstants } from './authentication.contants';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthenticationService, ...userProivders],
 })
 export class AuthenticationModule {}
