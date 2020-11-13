bf11485867a7376a9572c890e9cee8f72bc64106
bxine <sine.narisa@gmail.com>
2020-11-14 02:45:11 +0700

    Edit base JwtModule by Yee

--- src/common/authentication/authentication.module.ts
+++ src/common/authentication/authentication.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from 'nestjs-config/dist/module/config.service';
+import { ConfigModule } from '../../config/config.module';
 import { UserModule } from '../../user/user.module';
 import { userProivders } from '../../user/user.providers';
+import { AuthenticationConfig } from './authentication.config';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      //secret: jwtConstants.secret,
-      secret: ConfigService.get('JWT_SECRET'),
-      signOptions: {
-        expiresIn: `${ConfigService.get('JWT_EXPIRATION_TIME')}s`,
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useClass: AuthenticationConfig,
     }),
   ],
   providers: [AuthenticationService, ...userProivders],
