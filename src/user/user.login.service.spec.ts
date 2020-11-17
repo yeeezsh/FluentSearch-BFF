c5ff7df392e107863ab1caa0c496877ae083c7d2
bxine <sine.narisa@gmail.com>
2020-11-17 17:39:36 +0700

    [WIP] Fix compare error

--- src/user/user.login.service.spec.ts
+++ src/user/user.login.service.spec.ts
@@ -1,56 +1,64 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import * as mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 import { DATABASE_CONNECTION } from '../database/constants/database.constant';
-import { DatabaseModule } from '../database/database.module';
-import { mockDatabaseFactory, replSet } from '../utils/mock-database.factory';
+import { CreateUserDto } from './@dtos/user.dto';
 import { UserLoginDto } from './@dtos/user.login.dto';
+import { USER_MODEL } from './constants/user.provider.constant';
 import { UserLoginService } from './user.login.service';
-import { userProivders } from './user.providers';
 import { UserService } from './user.service';
 
-describe('UserService tests', () => {
+describe('UserLoginService tests', () => {
   let service: UserLoginService;
-  //let id: Types.ObjectId;
+  let userService: UserService;
+  let email: String;
   let module: TestingModule;
 
-  //   const expectedEmailTest = 'test@mail.com';
-  //   const mockCreateUserDto: CreateUserDto = {
-  //     mainEmail: expectedEmailTest,
-  //     name: 'test name',
-  //     password: '12345678',
-  //   };
+  const expectedEmailTest = 'test@mail.com';
+  const mockCreateUserDto: CreateUserDto = {
+    mainEmail: expectedEmailTest,
+    name: 'test name',
+    password: '12345678',
+  };
 
-  const mockLogin: UserLoginDto = {
-    email: 'test@mail.com',
+  const mockUserLogin: UserLoginDto = {
+    email: expectedEmailTest,
     password: '12345678',
   };
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports: [DatabaseModule],
-      providers: [...userProivders, UserService],
+      providers: [UserLoginService],
     })
       .overrideProvider(DATABASE_CONNECTION)
-      .useFactory({
-        factory: async () => mockDatabaseFactory(),
+      .useValue({})
+      .overrideProvider(USER_MODEL)
+      .useValue({
+        findOne: jest.fn().mockImplementation(() => {}),
       })
       .compile();
 
+    jest.spyOn(bcrypt, 'compare').mockImplementation(() => true);
+
+    userService = module.get<UserService>(UserService);
     service = module.get<UserLoginService>(UserLoginService);
-    //const user = await service;
+    const user = await userService.createUser(mockCreateUserDto);
+    email = user.mainEmail;
   });
 
-  afterAll(async () => {
-    await module.close();
-    await mongoose.disconnect();
-    await replSet.stop();
-  });
+  // afterAll(async () => {
+  //   await module.close();
+  //   await mongoose.disconnect();
+  //   await replSet.stop();
+  // });
 
   it('Should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  //   it('Should be return detail of user by input email', () => {
-  //     expect();
-  //   });
+  //loginUser
+  //FIXME: "TypeError: Cannot read property 'compare' of undefined" before it
+  it('Should be return detail of user', async () => {
+    const loginUser = await service.userLogin(mockUserLogin);
+    expect(loginUser?.mainEmail).toEqual(email);
+  });
 });
