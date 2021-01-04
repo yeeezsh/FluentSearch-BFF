16e3dc5740e846d088d489063358b021015bf9c9
yee2542 <yee2542gtl@gmail.com>
2021-01-05 00:07:59 +0700

    Implment skip & limit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  InternalServerErrorException,
+  ParseIntPipe,
+  Post,
+  Query,
+  UsePipes,
+} from '@nestjs/common';
+import { MongoHandlingEnum } from '../common/exception/@enums/mongo-handling.enum';
+import { MongoErrorException } from '../common/exception/mongo-error.exception';
 import { UsersQuery } from './@types/user.query.types';
 import { CreateUserDto } from './dtos/user.dto';
 import { UserTrimPipe } from './pipes/user.trim.pipe';
@@ -8,16 +20,20 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/')
-  async getUsers(): Promise<UsersQuery> {
-    return this.userService.getUsers();
+  @Get('/lists')
+  async getUsers(
+    @Query('skip', ParseIntPipe) skip: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ): Promise<UsersQuery> {
+    return this.userService.getUsers(skip, limit);
   }
 
   @Post('/register')
   @UsePipes(new UserTrimPipe())
   async createUser(@Body() body: CreateUserDto): Promise<void> {
-    await this.userService.createUser(body);
-    return;
+    try {
+      await this.userService.createUser(body);
+      return;
     } catch (error) {
       const catchErr = (error as unknown) as MongoErrorException;
 
