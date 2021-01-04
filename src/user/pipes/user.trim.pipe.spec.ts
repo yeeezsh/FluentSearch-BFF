6885a0b8973f5c5a46eed5cc80d40125004b6d97
yee2542 <yee2542gtl@gmail.com>
2021-01-05 02:19:42 +0700

    Add user pipes test

---
+++ src/user/pipes/user.trim.pipe.spec.ts
@@ -0,0 +1,43 @@
+// import { ValidationPipe } from "@nestjs/common";
+
+import { ArgumentMetadata } from '@nestjs/common';
+import { CreateUserDto } from '../dtos/user.dto';
+import { UserTrimPipe } from './user.trim.pipe';
+
+class CreateUserDtoTest implements CreateUserDto {
+  mainEmail!: string;
+  name!: string;
+  password!: string;
+}
+
+const metadata: ArgumentMetadata = {
+  type: 'body',
+  metatype: CreateUserDtoTest,
+  data: '',
+};
+
+describe('User trim pipe', () => {
+  let target: UserTrimPipe;
+
+  beforeAll(() => {
+    target = new UserTrimPipe();
+  });
+
+  it('Should trim space in mainEmail', async () => {
+    const testPayload = { mainEmail: 'test@test.com   ', password: '123456' };
+    const expectedPayload = { mainEmail: 'test@test.com', password: '123456' };
+    const result = await target.transform(testPayload, metadata);
+    expect(result).toEqual(expectedPayload);
+  });
+
+  it('Should not trim password', async () => {
+    const testPayload = {
+      mainEmail: 'test@test.com     ',
+      password: '123456    ',
+    };
+    const expectedPayload = testPayload;
+    const result = await target.transform(testPayload, metadata);
+    expect(result.mainEmail.length).toBe(13);
+    expect(result.password).toEqual(expectedPayload.password);
+  });
+});
