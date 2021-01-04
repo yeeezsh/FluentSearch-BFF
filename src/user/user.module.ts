5d8b18bbc03a9b18e3b52a846b328c4217ba8240
yee2542 <yee2542gtl@gmail.com>
2021-01-05 04:04:25 +0700

    Fix bad import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
 import { UserController } from './user.controller';
 import { userProivders } from './user.providers';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ConfigModule],
   controllers: [UserController],
   providers: [UserService, ...userProivders],
 })
