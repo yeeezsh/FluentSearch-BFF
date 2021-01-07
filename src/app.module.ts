16bc9eb8224955734af081e3dc3ed8a434547be1
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-01-07 14:37:06 +0700

    GraphQL Integration (#18)
    * Add graphql deps
    
    * implement graphql module
    
    * Add sort schema
    
    * Add root resolver
    
    * Add schema
    
    * Add return type
    
    * format
    
    * Add app resolver test
    
    * Refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
