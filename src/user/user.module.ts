ae7a343942269f7b8a8ce9bdc4466922b9ae2706
yee2542 <yee2542gtl@gmail.com>
2021-01-07 15:37:03 +0700

    Implement resolver

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { ConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
 import { UserController } from './user.controller';
 import { userProivders } from './user.providers';
+import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
   imports: [DatabaseModule, ConfigModule],
   controllers: [UserController],
-  providers: [UserService, ...userProivders],
+  providers: [UserService, UserResolver, ...userProivders],
 })
 export class UserModule {}
