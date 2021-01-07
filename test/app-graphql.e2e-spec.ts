84886144bc5fb3aa901d92d80bde2d80c1b22252
yee2542 <yee2542gtl@gmail.com>
2021-01-07 16:44:32 +0700

    clode db after test

--- test/app-graphql.e2e-spec.ts
+++ test/app-graphql.e2e-spec.ts
@@ -1,11 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import * as mongoose from 'mongoose';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { AppModel } from '../src/app.resolver';
 import { DATABASE_CONNECTION } from '../src/database/constants/database.constant';
-import { mockDatabaseFactory } from '../src/utils/mock-database.factory';
-
+import {
+  mockDatabaseFactory,
+  replSet,
+} from '../src/utils/mock-database.factory';
 describe('AppResolver GraphQL', () => {
   let app: INestApplication;
 
@@ -21,6 +24,12 @@ describe('AppResolver GraphQL', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await replSet.stop();
+    await app.close();
   });
 
   it('Query server status', () => {
