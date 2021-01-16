229196511d498a7e6412c237337e5ff1af6cdcb6
yee2542 <yee2542gtl@gmail.com>
2021-01-17 06:03:58 +0700

    fix bad user input

--- src/user/dtos/inputs/user-update.input.ts
+++ src/user/dtos/inputs/user-update.input.ts
@@ -1,8 +1,26 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsOptional, MaxLength, MinLength } from 'class-validator';
 import { UserRegisterInput } from './user-register.input';
 
 @InputType()
-export class UserUpdateInput extends UserRegisterInput {
+export class UserUpdateInput implements UserRegisterInput {
   @Field()
   id: string;
+
+  @Field({ nullable: true })
+  @IsEmail()
+  @IsOptional()
+  mainEmail: string;
+
+  @Field({ nullable: true })
+  @MinLength(6)
+  @MaxLength(48)
+  @IsOptional()
+  password: string;
+
+  @Field({ nullable: true })
+  @MinLength(1)
+  @MaxLength(48)
+  @IsOptional()
+  name: string;
 }
