099186fa7124f8600af51d45fdc33ceb1efe7e58
yee2542 <yee2542gtl@gmail.com>
2021-01-17 15:16:05 +0700

    add interface to implement

--- src/user/models/user-token.model.ts
+++ src/user/models/user-token.model.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ObjectType } from '@nestjs/graphql';
+import { IUserToken } from '../interfaces/user-token';
 import { OAuthProviderEnum } from '../schemas/enums/oauth-provider.enum';
 
 @ObjectType()
-export class UserToken {
+export class UserToken implements IUserToken {
   @Field(() => String)
   provider: OAuthProviderEnum;
 
