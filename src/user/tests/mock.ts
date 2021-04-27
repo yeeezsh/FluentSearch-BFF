0489a35201aa2391b896bde70d366bc06a2b3c79
yee2542 <yee2542gtl@gmail.com>
2021-04-27 21:14:35 +0700

    refactor

---
+++ src/user/tests/mock.ts
@@ -0,0 +1,48 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { IUser } from '../interfaces/user';
+import { User } from '../models/user.model';
+import { UserPackageEnum } from '../schemas/enums/user-package.enum';
+import { UserRoleEnum } from '../schemas/enums/user-role.enum';
+import { UserZoneEnum } from '../schemas/enums/user.zone.enum';
+
+export const MOCK_USER_DOCUMENT = {
+  _id: Types.ObjectId(),
+  mainEmail: 'john@doe.com',
+  email: ['john@doe.com'],
+  password: '12345678',
+  name: 'John',
+
+  oauth: [],
+
+  // logic
+  role: UserRoleEnum.user,
+  package: UserPackageEnum.freeUser,
+
+  //   meta
+  zone: UserZoneEnum.TH1,
+
+  createDate: new Date(),
+  updateDate: new Date(),
+} as IUser & { _id: Types.ObjectId };
+
+// jest
+//     .fn()
+//     .mockImplementation(() => Promise.resolve(MOCK_USER_DOCUMENT)),
+
+export const MOCK_USER_VALUE = {
+  find: jest.fn().mockReturnThis(),
+  skip: jest.fn().mockReturnThis(),
+  limit: jest.fn().mockReturnThis(),
+  create: jest.fn().mockReturnThis(),
+  findOneAndUpdate: jest.fn().mockReturnThis(),
+  findByIdAndUpdate: jest.fn().mockReturnThis(),
+  findById: jest.fn().mockReturnThis(),
+  select: jest.fn().mockReturnThis(),
+  lean: jest.fn().mockReturnThis(),
+};
+
+export const MOCK_USER_MODEL = {
+  provide: getModelToken(User.name),
+  useValue: MOCK_USER_VALUE,
+};
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,52 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import mongoose, { Types } from 'mongoose';
-import {
-  mongodbMockFactory,
-  replSet,
-} from '../../test/mock/mongodb.mock.factory';
+import { Types } from 'mongoose';
 import { UserNotExistsException } from '../common/exception/user-not-exists.exception';
 import { ConfigModule } from '../config/config.module';
-import { DATABASE_CONNECTION } from '../database/constants/database.constant';
-import { DatabaseModule } from '../database/database.module';
-import { UserRegisterInput } from './dtos/inputs/user-register.input';
-import { userProviders } from './user.providers';
+import {
+  MOCK_USER_DOCUMENT,
+  MOCK_USER_MODEL,
+  MOCK_USER_VALUE,
+} from './tests/mock';
 import { UserService } from './user.service';
 
 describe('UserService tests', () => {
   let service: UserService;
-  let database: Promise<typeof mongoose>;
   let userId: Types.ObjectId;
   let module: TestingModule;
 
-  const expectedEmailTest = 'test@mail.com';
-  const mockCreateUserDto: UserRegisterInput = {
-    mainEmail: expectedEmailTest,
-    name: 'test name',
-    password: '12345678',
-  };
+  // const expectedEmailTest = 'test@mail.com';
+  // const mockCreateUserDto: UserRegisterInput = {
+  //   mainEmail: expectedEmailTest,
+  //   name: 'test name',
+  //   password: '12345678',
+  // };
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports: [DatabaseModule, ConfigModule],
-      providers: [...userProviders, UserService],
-    })
-      .overrideProvider(DATABASE_CONNECTION)
-      .useFactory({
-        factory: async () => await mongodbMockFactory(),
-      })
-      .compile();
+      imports: [ConfigModule],
+      providers: [MOCK_USER_MODEL, UserService],
+    }).compile();
 
     service = module.get<UserService>(UserService);
-    database = module.get<Promise<typeof mongoose>>(DATABASE_CONNECTION);
-
-    const user = await service.createUser(mockCreateUserDto);
-    userId = user._id;
-  });
-
-  afterAll(async () => {
-    await module.close();
-    (await database).connection.close();
-    await replSet.stop();
   });
 
   it('Should be defined', () => {
@@ -55,49 +36,58 @@ describe('UserService tests', () => {
 
   // findById
   it('Should return user that find by id', async () => {
+    jest.spyOn(MOCK_USER_VALUE, 'lean').mockResolvedValue(MOCK_USER_DOCUMENT);
+
     const user = await service.findById(userId);
-    expect(user?.mainEmail).toEqual(expectedEmailTest);
+    expect(user?.mainEmail).toEqual(MOCK_USER_DOCUMENT.mainEmail);
   });
 
   // createUser
   it('Should create a user correctly', async () => {
-    const expectedEmailTest2 = 'test2@mail.com';
-    const doc = await service.createUser({
-      ...mockCreateUserDto,
-      mainEmail: expectedEmailTest2,
-    });
+    jest.spyOn(MOCK_USER_VALUE, 'lean').mockResolvedValue(MOCK_USER_DOCUMENT);
 
-    const userDoc = await service.findById(doc._id);
-
-    expect(userDoc?.mainEmail).toEqual(expectedEmailTest2);
+    const userDoc = await service.findById(MOCK_USER_DOCUMENT._id);
+    expect(userDoc?.mainEmail).toEqual(MOCK_USER_DOCUMENT.mainEmail);
   });
 
   // update user
   it('Should update a user correctly', async () => {
-    const expectedEmailTest3 = 'test3@mail.com';
+    jest
+      .spyOn(MOCK_USER_VALUE, 'findByIdAndUpdate')
+      .mockResolvedValue(MOCK_USER_DOCUMENT);
+
     const doc = await service.updateUser({
-      ...mockCreateUserDto,
-      id: (userId as unknown) as string,
-      mainEmail: expectedEmailTest3,
+      id: MOCK_USER_DOCUMENT._id.toHexString(),
+      name: 'K',
+      mainEmail: 'hi@hello.com',
     });
-    expect(doc?.mainEmail).toEqual(expectedEmailTest3);
+    expect(doc?.mainEmail).toEqual(MOCK_USER_DOCUMENT.mainEmail);
   });
 
   // update user
   it('Should throw when update bad user id', async () => {
-    const expectedEmailTest3 = 'test3@mail.com';
     const notExistId = Types.ObjectId();
+    jest
+      .spyOn(MOCK_USER_VALUE, 'findByIdAndUpdate')
+      .mockImplementationOnce(() => {
+        throw new UserNotExistsException();
+      });
+
     await expect(async () => {
       await service.updateUser({
-        ...mockCreateUserDto,
         id: notExistId.toHexString(),
-        mainEmail: expectedEmailTest3,
+        name: 'K',
+        mainEmail: 'hi@hello.com',
       });
     }).rejects.toThrow(UserNotExistsException);
   });
 
   // getUsers
   it('Should return all user', async () => {
+    jest
+      .spyOn(MOCK_USER_VALUE, 'lean')
+      .mockResolvedValue([MOCK_USER_DOCUMENT, MOCK_USER_DOCUMENT]);
+
     const users = await service.getUsers();
     expect(users.length).toBe(2);
   });
