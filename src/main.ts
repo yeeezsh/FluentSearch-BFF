155f0e417949fd768a79e9caa4b4b6b49115984b
yee2542 <yee2542gtl@gmail.com>
2021-05-17 00:18:07 +0700

    show env config when starting

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +12,7 @@ async function bootstrap() {
     .select(ConfigModule)
     .get(ConfigService)
     .get();
+  Logger.log(config);
 
   app.enableCors({
     origin: [config.origin],
