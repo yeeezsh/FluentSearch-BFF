bc4a7eb0398fc0b595c7c7bf681e72679a7cf1af
yee2542 <yee2542gtl@gmail.com>
2021-05-16 17:57:04 +0700

    complete auth service

--- src/authentication/authentication.service.spec.ts
+++ src/authentication/authentication.service.spec.ts
@@ -1,18 +1,46 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import bcryptjs from 'bcryptjs';
+import { Request, Response } from 'express';
+import { MOCK_USER_DOCUMENT, MOCK_USER_SERVICE } from '../user/tests/mock';
+import { UserService } from '../user/user.service';
 import { AuthenticationService } from './authentication.service';
+import { UserLoginInputDTO } from './dtos/user-login.input.dto';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
-    }).compile();
+      imports: [JwtModule.register({ secret: 'mockSecret' })],
+      providers: [AuthenticationService, UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(MOCK_USER_SERVICE)
+      .compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('when login() it should call jwt and user service', async () => {
+    const mockReq = ({
+      session: {
+        user: MOCK_USER_DOCUMENT,
+      },
+    } as unknown) as Request<any>;
+    const mockRes = {} as Response;
+    const mockArgs = {
+      password: 'mockpassword',
+    } as UserLoginInputDTO;
+
+    jest.spyOn(bcryptjs, 'compare').mockImplementation(() => true);
+    await service.userLogin(mockReq, mockRes, mockArgs);
+    const expectedCalled = jest.spyOn(MOCK_USER_SERVICE, 'getUserByEmail');
+
+    expect(expectedCalled).toBeCalled();
   });
 });
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcryptjs';
+import { Request, Response } from 'express';
+import { UserInvalidCredentialException } from '../common/exception/user.invalid-credential.exception';
+import { UserService } from '../user/user.service';
+import { UserLoginInputDTO } from './dtos/user-login.input.dto';
+import { UserSessionDTO } from './dtos/user-session.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async userLogin(
+    req: Request,
+    res: Response,
+    args: UserLoginInputDTO,
+  ): Promise<UserSessionDTO> {
+    const user = await this.userService.getUserByEmail(args.email);
+    const valid = await compare(args.password, user.password);
+    if (!valid) throw new UserInvalidCredentialException();
+
+    const parsed = {
+      ...user,
+      password: undefined,
+    };
+    const token = await this.jwtService.signAsync(parsed);
+
+    req.res?.cookie('Authorization', `Bearer ${token}`, {
+      httpOnly: true,
+    });
+    const session = req.session as Record<string, any>;
+    session.user = parsed;
+
+    return {
+      ...user,
+      _id: user._id,
+    };
+  }
+}
