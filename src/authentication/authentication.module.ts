3a0e4748829a1fa902beea7812e0ae357e1ed009
yee2542 <yee2542gtl@gmail.com>
2021-05-16 18:28:53 +0700

    refactor

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -6,21 +6,19 @@ import { UserModule } from '../user/user.module';
 import { AuthenticationResolver } from './authentication.resolver';
 import { AuthenticationService } from './authentication.service';
 
+const JwtModuleBase = JwtModule.registerAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    secret: config.get().jwt.secretKey,
+    signOptions: {
+      expiresIn: config.get().jwt.expires,
+    },
+  }),
+});
 @Module({
-  imports: [
-    ConfigModule,
-    UserModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        secret: config.get().jwt.secretKey,
-        signOptions: {
-          expiresIn: config.get().jwt.expires,
-        },
-      }),
-    }),
-  ],
+  imports: [ConfigModule, UserModule, JwtModuleBase],
   providers: [AuthenticationService, AuthenticationResolver],
+  exports: [JwtModuleBase, AuthenticationService, AuthenticationResolver],
 })
 export class AuthenticationModule {}
