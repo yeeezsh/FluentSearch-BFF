9c8b2f8d1e48ba9ac5f9aba07e8a7dffd4186b42
yee2542 <yee2542gtl@gmail.com>
2021-05-16 17:57:50 +0700

    register auth service

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
 import { UserModule } from '../user/user.module';
 import { AuthenticationResolver } from './authentication.resolver';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule,
+    UserModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get().jwt.secretKey,
+        signOptions: {
+          expiresIn: config.get().jwt.expires,
+        },
+      }),
+    }),
+  ],
   providers: [AuthenticationService, AuthenticationResolver],
 })
 export class AuthenticationModule {}
