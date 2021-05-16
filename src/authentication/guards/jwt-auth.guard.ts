d84a230e520e385355e5602c52037893a4d2906f
yee2542 <yee2542gtl@gmail.com>
2021-05-16 17:29:25 +0700

    add guard

---
+++ src/authentication/guards/jwt-auth.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { UserSessionDto } from 'fluentsearch-types';
+import { ConfigService } from '../../config/config.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+  canActivate(context: ExecutionContext): boolean {
+    const ctx = GqlExecutionContext.create(context);
+    const req = ctx.getContext().req as Request;
+    const token = req.cookies.Authorization || req.headers.Authorization;
+
+    if (!token) return false;
+
+    const extractToken = token.replace('Bearer ', '');
+    const { _id } = this.jwtService.verify(extractToken, {
+      secret: this.configService.get().jwt.secretKey,
+    }) as Pick<UserSessionDto, '_id'>;
+    const valid = !!_id;
+    return valid;
+  }
+}
