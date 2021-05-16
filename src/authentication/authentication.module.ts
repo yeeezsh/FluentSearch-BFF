2ce5b6b8399564da7de47847dc290a7bf59db546
yee2542 <yee2542gtl@gmail.com>
2021-05-16 16:11:41 +0700

    import user module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { AuthenticationResolver } from './authentication.resolver';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
+  imports: [UserModule],
   providers: [AuthenticationService, AuthenticationResolver],
 })
 export class AuthenticationModule {}
