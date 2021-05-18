ccf16564ba5a68417c65e1709bd05f133eb5a3c8
yee2542 <yee2542gtl@gmail.com>
2021-05-19 06:31:51 +0700

    register resolver

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
+import { FilesResolver } from './files.resolver';
 import { FilesService } from './files.service';
 
 @Module({
-  providers: [FilesService]
+  imports: [ConfigModule],
+  providers: [FilesService, FilesResolver],
 })
 export class FilesModule {}
