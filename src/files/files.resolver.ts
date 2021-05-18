017db70cc98a535758b9293d1366d57d772bfb31
yee2542 <yee2542gtl@gmail.com>
2021-05-19 06:52:50 +0700

    implement mock

--- src/files/files.resolver.ts
+++ src/files/files.resolver.ts
@@ -1,4 +1,61 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { SkipLimitArgs } from '../user/dtos/args/skip-limit.args';
+import { RecentFiles } from './dtos/recent-files.dto';
 
 @Resolver()
-export class FilesResolver {}
+export class FilesResolver {
+  @Query(() => RecentFiles)
+  async getRecentFiles(@Args() skipLimit: SkipLimitArgs): Promise<RecentFiles> {
+    return {
+      result: [
+        {
+          date: new Date(),
+          files: [
+            {
+              label: 'test.jpeg',
+              uri: 'http://storage.fluentsearch.ml/123/456',
+              uri_thumbnail: 'http://storage.fluentsearch.ml/123/456',
+              createAt: new Date(),
+              updateAt: new Date(),
+            },
+
+            {
+              label: 'test1.jpeg',
+              uri: 'http://storage.fluentsearch.ml/123/456',
+              uri_thumbnail: 'http://storage.fluentsearch.ml/123/456',
+              createAt: new Date(),
+              updateAt: new Date(),
+            },
+            {
+              label: 'test2.jpeg',
+              uri: 'http://storage.fluentsearch.ml/123/456',
+              uri_thumbnail: 'http://storage.fluentsearch.ml/123/456',
+              createAt: new Date(),
+              updateAt: new Date(),
+            },
+          ],
+        },
+        {
+          date: new Date('01/01/1999'),
+          files: [
+            {
+              label: 'test.jpeg',
+              uri: 'http://storage.fluentsearch.ml/123/456',
+              uri_thumbnail: 'http://storage.fluentsearch.ml/123/456',
+              createAt: new Date(),
+              updateAt: new Date(),
+            },
+
+            {
+              label: 'test1.jpeg',
+              uri: 'http://storage.fluentsearch.ml/123/456',
+              uri_thumbnail: 'http://storage.fluentsearch.ml/123/456',
+              createAt: new Date(),
+              updateAt: new Date(),
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
