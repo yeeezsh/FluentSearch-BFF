ec2d1c6b256433bde3fa728619dd51f22e205ea2
yee2542 <yee2542gtl@gmail.com>
2021-05-23 06:54:57 +0700

    add get recent file service

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FileDocument, FILES_SCHEMA_NAME } from 'fluentsearch-types';
+import { Model } from 'mongoose';
+import { ConfigService } from '../config/config.service';
+import { RecentPreviews } from './dtos/recent-files.dto';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor(
+    @InjectModel(FILES_SCHEMA_NAME)
+    private readonly userModel: Model<FileDocument>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getRecentFilesByUser(userId: string, skip: number, limit: number) {
+    const result = await this.userModel
+      .aggregate<RecentPreviews>([
+        { $match: { owner: userId } },
+        { $sort: { createAt: -1 } },
+        { $skip: skip },
+        { $limit: limit },
+
+        //   file
+        {
+          $addFields: {
+            uri: {
+              $concat: [
+                `http://${this.configService.get().storage_endpoint}/`,
+                '$owner',
+                '/',
+                {
+                  $toString: '$_id',
+                },
+              ],
+            },
+            uri_thumbnail: {
+              $concat: [
+                `http://${this.configService.get().storage_endpoint}/`,
+                '$owner',
+                '/',
+                {
+                  $toString: '$_id',
+                },
+              ],
+            },
+          },
+        },
+
+        //   select
+        {
+          $project: {
+            _id: 1,
+            owner: 1,
+            original_filename: 1,
+            uri: 1,
+            uri_thumbnail: 1,
+            createAt: 1,
+          },
+        },
+
+        //   group to date
+        {
+          $addFields: {
+            date: { $dateToString: { format: '%d-%m-%Y', date: '$createAt' } },
+          },
+        },
+        {
+          $group: {
+            _id: '$date',
+
+            files: { $push: '$$ROOT' },
+          },
+        },
+        {
+          $addFields: {
+            date: '$_id',
+          },
+        },
+      ])
+      .allowDiskUse(true);
+
+    return result;
+  }
+}
