ad7b50343f5b9c3815e34763e4afee043684fcef
yee2542 <yee2542gtl@gmail.com>
2021-05-23 05:04:44 +0700

    remove schema

--- src/user/schemas/enums/oauth-provider.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum OAuthProviderEnum {
-  facebook = 'facebook',
-}
-
-export const oAuthProviderConstant: OAuthProviderEnum[] = [
-  OAuthProviderEnum.facebook,
-];
--- src/user/schemas/enums/user-package.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum UserPackageEnum {
-  freeUser = 'free-user',
-  paidUser = 'paid-user',
-}
-
-export const userPackageConstant: UserPackageEnum[] = [
-  UserPackageEnum.freeUser,
-  UserPackageEnum.paidUser,
-];
