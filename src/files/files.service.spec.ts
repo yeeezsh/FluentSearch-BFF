a240fe399c4744a120e9cd6c3cd081d0e15d1a2a
yee2542 <yee2542gtl@gmail.com>
2021-05-23 06:59:02 +0700

    fix bad test

--- src/files/files.service.spec.ts
+++ src/files/files.service.spec.ts
@@ -1,4 +1,7 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { FILES_SCHEMA_NAME } from 'fluentsearch-types';
+import { ConfigModule } from '../config/config.module';
 import { FilesService } from './files.service';
 
 describe('FilesService', () => {
@@ -6,7 +9,11 @@ describe('FilesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilesService],
+      imports: [ConfigModule],
+      providers: [
+        FilesService,
+        { provide: getModelToken(FILES_SCHEMA_NAME), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<FilesService>(FilesService);
