1cd2d4f1ab5895bdabac24c1f5203ecb5dce7ce0
yee2542 <yee2542gtl@gmail.com>
2021-05-23 05:54:33 +0700

    register file schema

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FILES_SCHEMA_NAME } from 'fluentsearch-types';
+import fileSchema from 'fluentsearch-types/dist/entity/file.entity';
 import { ConfigModule } from '../config/config.module';
 import { FilesResolver } from './files.resolver';
 import { FilesService } from './files.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
+      {
+        name: FILES_SCHEMA_NAME,
+        schema: fileSchema,
+      },
+    ]),
+  ],
   providers: [FilesService, FilesResolver],
 })
 export class FilesModule {}
