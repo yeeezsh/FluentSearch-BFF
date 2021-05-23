6616f826d5052884dac1617edcf455a819183d95
yee2542 <yee2542gtl@gmail.com>
2021-05-23 20:45:19 +0700

    refactor

--- src/files/dtos/file/file.dto.model.ts
+++ src/files/dtos/file/file.dto.model.ts
@@ -3,63 +3,12 @@ import {
   BaseFileMetaSchema,
   BaseFileSchema,
   FileTypeEnum,
-  VideoMeta,
   ZoneEnum,
 } from 'fluentsearch-types';
+import { FileMetaDTO } from './file-meta.dto';
 
 registerEnumType(FileTypeEnum, { name: 'FileTypeEnum' });
 registerEnumType(ZoneEnum, { name: 'ZoneEnum' });
-type VideoDurationType = VideoMeta['duration'];
-
-@ObjectType()
-export class FileDurationMetaDTO implements VideoDurationType {
-  @Field()
-  original: string;
-
-  @Field()
-  hour: number;
-
-  @Field()
-  minute: number;
-
-  @Field()
-  second: number;
-}
-
-@ObjectType()
-export class FileMetaDTO implements BaseFileMetaSchema<Record<string, any>> {
-  @Field()
-  size: number;
-
-  @Field()
-  width: number;
-
-  @Field()
-  height: number;
-
-  @Field()
-  extension: string;
-
-  @Field()
-  contentType: string;
-
-  @Field(() => String, { nullable: true })
-  sha1?: string;
-
-  //   video meta
-
-  @Field(() => Number, { nullable: true })
-  fps: number;
-
-  @Field(() => String, { nullable: true })
-  codec: string;
-
-  @Field(() => Number, { nullable: true })
-  bitrate: number;
-
-  @Field(() => FileDurationMetaDTO, { nullable: true })
-  duration: FileDurationMetaDTO;
-}
 
 @ObjectType()
 export class FileModelDTO
