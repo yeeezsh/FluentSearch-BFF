beb76f7180ccf27acc4d0e4ba0f60ebf1fc96af8
yee2542 <yee2542gtl@gmail.com>
2021-05-23 20:45:43 +0700

    add GetFileById endpoint

--- src/files/files.resolver.ts
+++ src/files/files.resolver.ts
@@ -1,12 +1,34 @@
 import { Args, Context, Query, Resolver } from '@nestjs/graphql';
 import { Request } from 'express';
+import { ConfigService } from '../config/config.service';
 import { SkipLimitArgs } from '../user/dtos/args/skip-limit.args';
+import { FileModelDTO } from './dtos/file/file.dto.model';
 import { RecentFiles } from './dtos/recent-files.dto';
 import { FilesService } from './files.service';
 
 @Resolver()
 export class FilesResolver {
-  constructor(private readonly fileService: FilesService) {}
+  constructor(
+    private readonly fileService: FilesService,
+    private readonly configSerivce: ConfigService,
+  ) {}
+
+  @Query(() => FileModelDTO)
+  async GetFileById(
+    @Args('id') fileId: string,
+    @Context('req') req: Request,
+  ): Promise<FileModelDTO> {
+    const session = req.session as Record<string, any>;
+    const owner = session.user._id;
+    const file = await this.fileService.getFile(fileId, owner);
+    return {
+      ...file,
+      uri: `http://${this.configSerivce.get().storage_endpoint}/${owner}/${
+        file._id
+      }`,
+    };
+  }
+
   @Query(() => RecentFiles)
   async GetRecentFiles(
     @Args() skipLimit: SkipLimitArgs,
