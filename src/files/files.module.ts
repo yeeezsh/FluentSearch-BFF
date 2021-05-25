445a1768048f5c6cb85339e06cee6209564ef20c
yee2542 <yee2542gtl@gmail.com>
2021-05-26 06:55:40 +0700

    resolve deps

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FILES_SCHEMA_NAME } from 'fluentsearch-types';
 import fileSchema from 'fluentsearch-types/dist/entity/file.entity';
+import { AuthenticationModule } from '../authentication/authentication.module';
 import { ConfigModule } from '../config/config.module';
 import { FilesResolver } from './files.resolver';
 import { FilesService } from './files.service';
@@ -9,6 +10,7 @@ import { FilesService } from './files.service';
 @Module({
   imports: [
     ConfigModule,
+    AuthenticationModule,
     MongooseModule.forFeature([
       {
         name: FILES_SCHEMA_NAME,
